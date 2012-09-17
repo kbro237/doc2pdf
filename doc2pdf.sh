0b3470715603901bbc7e4393c92cf5190a825f4b
Keith Walbolt <keith.walbolt@gmail.com>
2012-09-17 12:12:37 -0400

    updated to work better and remove temp files

--- doc2pdf.sh
+++ doc2pdf.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-textutil -convert html *.doc*
+for crap in *.doc*
+do
+    textutil -convert html -output "$crap.temp.html" "$crap"
+done
 
-# mdfind -0 -onlyin . "kind:html" | xargs -0 -I {} /System/Library/Printers/Libraries/convert -f {} -o {}.pdf
-# mdfind -0 -onlyin . "kind:html" | xargs echo
-# find . -name "*.html" -print0 | xargs -0 -I {} /System/Library/Printers/Libraries/convert -f {} -o {}.pdf
-
-# this is almost there, but after the > I can't use the xargs variable anymore. Looks like I'll have to write a real script
-find . -name "*.html" -print0 | xargs -0 -I ^ /usr/sbin/cupsfilter ^ > "^.pdf"
+for temp in *.temp.html
+do
+    /usr/sbin/cupsfilter "$temp" > "`basename $temp | sed 's/\.temp\.html//'`.pdf"
+    rm "$temp"
+done
